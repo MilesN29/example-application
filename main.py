1e3cbc6feb167e518f1fda9e3158dab2d699dce0
MilesN29 <mdn29@pitt.edu>
2026-06-18 13:49:08 -0400

    broken app v6

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'v5.0.0 - Fixed version'  # Pretend this is broken
+    return 'v6.0.0 - Broken version again'  # Pretend this is broken
 
 app.run(host='0.0.0.0', port=8080)
