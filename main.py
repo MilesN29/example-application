00449fce73cb2e3282137cc5ec81670d994a452e
MilesN29 <mdn29@pitt.edu>
2026-06-18 13:44:30 -0400

    fix the code

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'v4.0.0 - BROKEN VERSION!'  # Pretend this is broken
+    return 'v5.0.0 - Fixed version'  # Pretend this is broken
 
 app.run(host='0.0.0.0', port=8080)
